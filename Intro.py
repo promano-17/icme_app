5d8323e0b0c1e06017c080c71e18f795dd309bc8
promano-17 <156252656+promano-17@users.noreply.github.com>
2025-11-11 13:35:06 -0500

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# st.set_page_config(
+#     page_title="CME Hunters",
+#     layout="wide",
+#     page_icon="☀️",
+# )
+
+st.title(r"☀️💨 $\textcolor{red}{\textbf{CME}}$ Hunters")
+st.header(r"🚀 Become a CME Hunter!")
+st.sidebar.success("Select a demo above.")
+
+st.markdown("This app teaches you:")
+
+st.markdown("1. to identify giant solar eruptions from the Sun 🌞, known as Coronal Mass Ejections (CMEs)")
+    
+st.markdown("2. how to help NASA researchers study and analyze them 🌞🔭")
+
+st.markdown("""### Watch this video first!""")
+
+st.markdown("**video here**")
+
+st.markdown("**👈 Select an activty.**")
